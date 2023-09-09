f8fe5cd0f31f20d9aa7dedf1a7d7ed8afbc164a2
Sutirtha <sdas@republicfinance.com>
2023-09-09 20:44:21 +0530

    ENH: create org

---
+++ app/create-org/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Page() {
+    return (
+      <div className="mt-24 flex items-start justify-center h-max">
+        Org Created
+      </div>
+    );
+  }
+  
--- components/CreateOrgCard.tsx
+++ components/CreateOrgCard.tsx
@@ -1,41 +1,43 @@
-import React from 'react'
+import React from "react";
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-  } from "@/components/ui/card";
-  import { Button } from "@/components/ui/button";
-  import { Input } from "@/components/ui/input";
-  import { Label } from "@/components/ui/label";
-  
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
 
 const CreateOrgCard = () => {
   return (
     <Card className="mx-10 w-80">
-        <CardHeader>
-          <CardTitle>Create Organization</CardTitle>
-          <CardDescription>
-            Create your organization and add properties for feedback management
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form>
-            <div className="grid w-full items-center gap-4">
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="name">Name</Label>
-                <Input id="name" placeholder="Name of your organization" />
-              </div>
+      <CardHeader>
+        <CardTitle>Create Organization</CardTitle>
+        <CardDescription>
+          Create your organization and add properties for feedback management
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" placeholder="Name of your organization" />
             </div>
-          </form>
-        </CardContent>
-        <CardFooter className="flex justify-center">
-          <Button className='w-24'>Create</Button>
-        </CardFooter>
-      </Card>
-  )
-}
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-center">
+        <Link href={`/create-org/`}>
+          <Button className="w-24">Create</Button>
+        </Link>
+      </CardFooter>
+    </Card>
+  );
+};
 
-export default CreateOrgCard
+export default CreateOrgCard;
